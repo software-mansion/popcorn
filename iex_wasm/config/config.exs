7f7d541f7c6cfe9e6969b44f0e21a868b3a294a4
Jakub Gonet <jakub.gonet@swmansion.com>
2025-01-21 21:09:43 +0100

    Add option to build test modules

--- iex_wasm/config/config.exs
+++ iex_wasm/config/config.exs
@@ -1,7 +1,24 @@
 import Config
 
+string_to_bool = fn
+  "true" -> true
+  "1" -> true
+  "0" -> false
+  "false" -> false
+end
+
+include_tracing = "EX_TRACING" |> System.get_env("true") |> string_to_bool.()
+
 config :fission_lib,
-  out_path: "static/elixir_modules.avm",
-  start_module: ElixirModules,
-  add_tracing: true,
+  out_path: "static/iex_wasm.avm",
+  start_module: IExWASM,
+  add_tracing: include_tracing,
   avm_source: {:git, "git@github.com:software-mansion-labs/FissionVM.git"}
+
+build_test_modules = "EX_MODULES" |> System.get_env("false") |> string_to_bool.()
+
+if build_test_modules do
+  config :fission_lib,
+    out_path: "static/elixir_modules.avm",
+    start_module: ElixirModules
+end
