aa0df664935ec8edeefd10883e15f9e1aa854721
Jakub Gonet <jakub.gonet@swmansion.com>
2025-01-21 02:11:05 +0100

    Add ElixirModules file for testing

--- iex_wasm/config/config.exs
+++ iex_wasm/config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :fission_lib,
-  out_path: "static/iex_wasm.avm",
-  start_module: IExWASM,
+  out_path: "static/elixir_modules.avm",
+  start_module: ElixirModules,
   add_tracing: true,
   avm_source: {:git, "git@github.com:software-mansion-labs/FissionVM.git"}
---
+++ iex_wasm/lib/elixir_modules.ex
@@ -0,0 +1,37 @@
+defmodule ElixirModules do
+  @factorial_module """
+  defmodule Factorial do
+    def calc(0), do: 1
+    def calc(n) when n > 0, do: n * calc(n - 1)
+  end
+  """
+
+  @fibonacci_modules """
+  defmodule Fibonacci do
+      def calc(0) do 0 end
+      def calc(1) do 1 end
+      def calc(n) do fib(n-1) + fib(n-2) end
+  end
+  """
+
+  def start() do
+    :elixir.start([], [])
+
+    (@fibonacci_modules <> "\nFibonacci.calc(10)\n")
+    |> eval()
+    |> print()
+  end
+
+  defp eval(string) do
+    string
+    |> Code.eval_string([], __ENV__)
+    |> elem(0)
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  defp print(term) do
+    :erlang.display({~c"____________ElixirModules____________", term})
+  end
+end
