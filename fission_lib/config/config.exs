457d9625eb3e842828463030234e0d3f285b34d3
Mateusz Front <mateusz.front@swmansion.com>
2025-03-13 12:34:21 +0100

    update FissionVM

--- fission_lib/config/config.exs
+++ fission_lib/config/config.exs
@@ -2,8 +2,7 @@ import Config
 
 config :fission_lib,
   add_tracing: false,
-  avm_source:
-    {:git, "git@github.com:software-mansion-labs/FissionVM.git", ref: "revert-104-gc-fibonacci"}
+  avm_source: {:git, "git@github.com:software-mansion-labs/FissionVM.git"}
 
 if File.exists?("#{__DIR__}/config.secret.exs") do
   # Put the following configuration in config.secret.exs:
