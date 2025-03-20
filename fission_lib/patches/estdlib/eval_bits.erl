5b0257a976e523bc6c2b876ce780654c57b462bc
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-03-20 11:50:33 +0100

    patch reason added in comments

--- fission_lib/patches/estdlib/eval_bits.erl
+++ fission_lib/patches/estdlib/eval_bits.erl
@@ -1,32 +1,11 @@
-%% -*- erlang-indent-level: 4 -*-
-%%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 1999-2022. All Rights Reserved.
-%% 
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%% 
-%% %CopyrightEnd%
-%%
 -module(eval_bits).
 
-%% Avoid warning for local function error/1 clashing with autoimported BIF.
--compile({no_auto_import,[error/1]}).
 -export([expr_grp/3,expr_grp/4]).
 
 -define(STACKTRACE,
         element(2, erlang:process_info(self(), current_stacktrace))).
 
+%% Patch reason: in AVM the option "-unit:Size" inside bitstrings is not supported
 expr_grp(Fields, Bindings, EvalFun, ErrorFun) ->
     expr_grp(Fields, Bindings, EvalFun, ErrorFun, <<>>).
 
