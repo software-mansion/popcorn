9d0596e4cd960650c49ce872b9df686b6926d3ce
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-04-11 12:28:21 +0200

    Deleted patch. Added test to cover filename:split/1 function call.

--- fission_lib/patches/estdlib/filename.erl
+++ fission_lib/patches/estdlib/filename.erl
@@ -1,18 +1,5 @@
 -module(filename).
--compile({flb_patch_private, unix_splitb/1}).
 -compile({flb_patch_private, filename_string_to_binary/1}).
-
-unix_splitb(Name) ->
-    %Patch reason: binary:split using list of separators.
-    L = binary:split(Name, <<"/">>, [global]),
-    LL =
-        case L of
-            [<<>> | Rest] when Rest =/= [] ->
-                [<<"/">> | Rest];
-            _ ->
-                L
-        end,
-    [X || X <- LL, X =/= <<>>].
 
 filename_string_to_binary(List) ->
     %Patch reason: changed unicode to utf8 due to lack of support.
