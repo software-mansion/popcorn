4adc42816c5780c55c8caaf025ee5c212befa5b7
FKubisSWM <franciszek.kubis@swmansion.com>
2025-04-15 12:53:34 +0200

    Removed compile:env_default_opts/0 patch (#90)
    cc: @bblaszkow06 @mat-hek

--- fission_lib/patches/estdlib/compile.erl
+++ fission_lib/patches/estdlib/compile.erl
@@ -2,7 +2,6 @@
 
 -module(compile).
 
--compile({flb_patch_private, env_default_opts/0}).
 -compile({flb_patch_private, debug_info_chunk/1}).
 -type option() :: atom() | {atom(), term()} | {'d', atom(), term()}.
 -type abstract_code() :: [erl_parse:abstract_form()].
@@ -28,33 +27,6 @@
     warnings = [] :: warnings(),
     extra_chunks = [] :: [{binary(), binary()}]
 }).
-
-%% Patch reason: os:getenv/1 is not supported.
-env_default_opts() ->
-    [].
-% env_default_opts() ->
-%     Key = "ERL_COMPILER_OPTIONS",
-%     case os:getenv(Key) of
-%         false ->
-%             [];
-%         Str when is_list(Str) ->
-%             case erl_scan:string(Str) of
-%                 {ok, Tokens, _} ->
-%                     Dot = {dot, erl_anno:new(1)},
-%                     case erl_parse:parse_term(Tokens ++ [Dot]) of
-%                         {ok, List} when is_list(List) -> List;
-%                         {ok, Term} ->
-%                             [Term];
-%                         {error, _Reason} ->
-%                             io:format("Ignoring bad term in ~s\n", [Key]),
-%                             []
-%                     end;
-%                 {error, {_, _, _Reason}, _} ->
-%                     io:format("Ignoring bad term in ~s\n", [Key]),
-%                     []
-%             end
-%     end.
-%% Patch reason: end
 
 debug_info_chunk(#compile{
     mod_options = ModOpts0,
