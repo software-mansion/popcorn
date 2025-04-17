bfe598e86296639126d23aec5cec5aa220d5433c
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-04-17 09:19:56 +0200

    Use OTP supervisor with hibernate patch (#88)
    This allows starting a supervisor with `:one_for_all` strategy, while not calling the not implemented `hibernate` BIF

--- fission_lib/patches/estdlib/supervisor.erl
+++ fission_lib/patches/estdlib/supervisor.erl
@@ -1,120 +1,62 @@
-%
-% This file is part of AtomVM.
-%
-% Copyright 2021 Davide Bettio <davide@uninstall.it>
-%
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-%
-%    http://www.apache.org/licenses/LICENSE-2.0
-%
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
-%
-% SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
-%
-
 -module(supervisor).
 
--behavior(gen_server).
+-compile({flb_patch_private, init_children/2}).
 
--export([
-    start_link/2, start_link/3
-]).
+-type auto_shutdown() :: never | any_significant | all_significant.
+-type child() :: undefined | pid().
+-type child_id() :: term().
+-type mfargs() :: {M :: module(), F :: atom(), A :: [term()] | undefined}.
+-type modules() :: [module()] | dynamic.
+-type restart() :: permanent | transient | temporary.
+-type significant() :: boolean().
+-type shutdown() :: brutal_kill | timeout().
+-type worker() :: worker | supervisor.
+-type strategy() :: one_for_all | one_for_one | rest_for_one | simple_one_for_one.
+-type children() :: {Ids :: [child_id()], Db :: #{child_id() => child_rec()}}.
 
--export([
-    init/1,
-    handle_call/3,
-    handle_cast/2,
-    handle_info/2
-]).
+-record(child,
+        {% pid is undefined when child is not running
+         pid = undefined :: child() | {restarting, pid() | undefined} | [pid()],
+         id :: child_id(),
+         mfargs :: mfargs(),
+         restart_type :: restart(),
+         significant :: significant(),
+         shutdown :: shutdown(),
+         child_type :: worker(),
+         modules = [] :: modules()}).
 
--record(child, {pid = undefined, id, mfargs, restart_type, shutdown, child_type, modules = []}).
--record(state, {children = []}).
+-type child_rec() :: #child{}.
 
-start_link(Module, Args) ->
-    gen_server:start_link(?MODULE, {Module, Args}, []).
-start_link(SupName, Module, Args) ->
-    gen_server:start_link(SupName, ?MODULE, {Module, Args}, []).
+-record(state,
+        {name,
+         strategy = one_for_one :: strategy(),
+         children = {[], #{}} :: children(), % Ids in start order
+         dynamics :: {maps, #{pid() => list()}} | {mapsets, #{pid() => []}} | undefined,
+         intensity = 1 :: non_neg_integer(),
+         period = 5 :: pos_integer(),
+         restarts = [],
+         dynamic_restarts = 0 :: non_neg_integer(),
+         auto_shutdown = never :: auto_shutdown(),
+         module,
+         args}).
 
-init({Mod, Args}) ->
-    erlang:process_flag(trap_exit, true),
-    case Mod:init(Args) of
-        {ok, {{one_for_one, _Intensity, _Period}, StartSpec}} ->
-            State = init_state(StartSpec, #state{}),
-            NewChildren = start_children(State#state.children, []),
-            {ok, State#state{children = NewChildren}};
-        Error ->
-            {stop, {bad_return, {mod, init, Error}}}
-    end.
-
-init_state([{ChildId, MFA, Restart, brutal_kill, Type, Modules} | T], State) ->
-    Child = #child{
-        id = ChildId,
-        mfargs = MFA,
-        restart_type = Restart,
-        shutdown = brutal_kill,
-        child_type = Type,
-        modules = Modules
-    },
-    NewChildren = [Child | State#state.children],
-    init_state(T, #state{children = NewChildren});
-init_state([], State) ->
-    State#state{children = lists:reverse(State#state.children)}.
-
-start_children([Child | T], StartedC) ->
-    #child{mfargs = {M, F, Args}} = Child,
-    case apply(M, F, Args) of
-        {ok, Pid} when is_pid(Pid) ->
-            start_children(T, [Child#child{pid = Pid} | StartedC])
-    end;
-start_children([], StartedC) ->
-    StartedC.
-
-restart_child(Pid, Reason, State) ->
-    Child = lists:keyfind(Pid, #child.pid, State#state.children),
-
-    #child{mfargs = {M, F, Args}} = Child,
-    case should_restart(Reason, Child#child.restart_type) of
-        true ->
-            case apply(M, F, Args) of
-                {ok, NewPid} when is_pid(Pid) ->
-                    NewChild = Child#child{pid = NewPid},
-                    Children = lists:keyreplace(Pid, #child.pid, State#state.children, NewChild),
-                    {ok, State#state{children = Children}}
-            end;
-        false ->
-            Children = lists:keydelete(Pid, #child.pid, State#state.children),
-            {ok, State#state{children = Children}}
-    end.
-
-should_restart(_Reason, permanent) ->
-    true;
-should_restart(_Reason, temporary) ->
-    false;
-should_restart(Reason, transient) ->
-    case Reason of
-        normal -> false;
-        _any -> true
-    end.
-
-handle_call(_Msg, _from, State) ->
-    {noreply, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info({'EXIT', Pid, Reason}, State) ->
-    case restart_child(Pid, Reason, State) of
-        {ok, State1} ->
-            {noreply, State1};
-        {shutdown, State1} ->
-            {stop, shutdown, State1}
-    end;
-handle_info(_Msg, State) ->
-    %TODO: log unexpected message
-    {noreply, State}.
+% Patch reason:
+% no support for `hibernate` option
+init_children(State, StartSpec) ->
+  SupName = State#state.name,
+  case flb_module:check_startspec(StartSpec, State#state.auto_shutdown) of
+    {ok, Children} ->
+      case flb_module:start_children(Children, SupName) of
+        {ok, NChildren} ->
+          %% Static supervisor are not expected to
+          %% have much work to do so hibernate them
+          %% to improve memory handling.
+          %% PATCH: removed hibernate
+          {ok, State#state{children = NChildren}};
+        {error, NChildren, Reason} ->
+          _ = flb_module:terminate_children(NChildren, SupName),
+          {stop, {shutdown, Reason}}
+      end;
+    Error ->
+      {stop, {start_spec, Error}}
+  end.
