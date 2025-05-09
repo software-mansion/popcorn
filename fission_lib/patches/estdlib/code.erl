af7d9bdae6280e83a32f2f5970636f021ceb0c3a
FKubisSWM <franciszek.kubis@swmansion.com>
2025-05-09 08:43:34 +0200

    Removed code:which/1 patch (#93)
    Removing of the patch waits for:
    https://github.com/software-mansion-labs/FissionVM/pull/127

--- fission_lib/patches/estdlib/code.erl
+++ fission_lib/patches/estdlib/code.erl
@@ -30,7 +30,6 @@
     load_abs/1,
     load_binary/3,
     ensure_loaded/1,
-    which/1,
     get_object_code/1
 ]).
 
@@ -100,10 +99,6 @@ load_binary(_Module, _Filename, _Binary) ->
     Module :: atom().
 ensure_loaded(_Module) ->
     erlang:nif_error(undefined).
-
-% Patch reason: mock implementation, prevents elixir_module:format_error/1 from crashing
-which(_Module) ->
-    non_existing.
 
 % Patch reason: mock implementation, prevents ParallelChecker.cache_module/2 from crashing
 get_object_code(_Module) ->
