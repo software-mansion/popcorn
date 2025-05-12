fdbdf2e32382bb3235106359129482fd76a9819c
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-05-12 15:18:21 +0200

    PoC of start module generation

--- lib/mix/tasks/popcorn.cook.ex
+++ lib/mix/tasks/popcorn.cook.ex
@@ -19,18 +19,53 @@ defmodule Mix.Tasks.Popcorn.Cook do
   @impl true
   def run(args) do
     parser_config = [
-      strict: [out_dir: :string, target: :string, start_module: :string],
-      aliases: [d: :out_dir, m: :start_module]
+      strict: [out_dir: :string, target: :string],
+      aliases: [d: :out_dir]
     ]
 
     {options, _rest} = OptionParser.parse!(args, parser_config)
 
-    options =
-      Enum.map(options, fn
-        {:start_module, module} -> {:start_module, Module.concat([module])}
-        entry -> entry
-      end)
+    app_entrypoint = get_starting_point()
+    entrypoint = create_starting_point(app_entrypoint)
+    options = Keyword.put(options, :start_module, entrypoint)
 
     Popcorn.cook(options)
+  end
+
+  defp get_starting_point() do
+    config = Mix.Project.config()
+    app = Keyword.get(config, :app)
+
+    case Application.spec(app, :mod) do
+      {_mod, _args} = app_mod ->
+        app_mod
+
+      _ ->
+        raise "Missing application starting point. Please provide `:mod` in applocation config of your `mix.exs`"
+    end
+  end
+
+  # module is expected to implement Application behaviour
+  defp create_starting_point({module, args}) do
+    contents =
+      quote do
+        def start() do
+          :erlang.display("Entrystart")
+          unquote(module).start(:normal, unquote(args))
+          :ok
+        end
+      end
+
+    Mix.shell().info(Macro.to_string(contents))
+    module_name = Popcorn.Entrypoint
+
+    # TODO: `mix compile.app` will autogenerate `:modules` key for app,
+    # resulting in this module being autoloaded and then trigger a warning about module redefinition
+    {:module, _module_name, binary_content, _term} =
+      Module.create(module_name, contents, Macro.Env.location(__ENV__))
+
+    filename = Mix.Project.compile_path() |> Path.join("#{module_name}.beam")
+    File.write!(filename, binary_content)
+    module_name
   end
 end
