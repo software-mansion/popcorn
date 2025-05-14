64ed6a6d9580b91c3b8f8979f45b62872689e0b4
Jakub Gonet <jakub.gonet@swmansion.com>
2025-05-14 10:07:08 +0200

    API fixes (#107)
    - Fix method of gathering exported beams from Popcorn library
    - Prefix internal events with _popcorn in Wasm.ex
    - Fix game of life popcorn dep location

--- examples/game_of_life/mix.exs
+++ examples/game_of_life/mix.exs
@@ -26,7 +26,7 @@ defmodule GameOfLife.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:popcorn, path: "../popcorn"}
+      {:popcorn, path: "../.."}
     ]
   end
 end
