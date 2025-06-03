a4e9c95b321c1afff590280a90907ab671498721
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-06-03 14:45:50 +0200

    Don't close AtomVM after app start. Handle optional deps, and common errors.

--- lib/mix/tasks/popcorn.cook.ex
+++ lib/mix/tasks/popcorn.cook.ex
@@ -58,7 +58,19 @@ defmodule Mix.Tasks.Popcorn.Cook do
           :ok = :application.start_boot(:stdlib, :permanent)
 
           {:ok, _apps} = Application.ensure_all_started(unquote(app), :permanent)
-          :ok
+
+          case :application.get_supervisor(unquote(app)) do
+            :undefined ->
+              :ok
+
+            {:ok, pid} ->
+              ref = Process.monitor(pid)
+
+              receive do
+                {:DOWN, ^ref, :process, _object, reason} ->
+                  reason
+              end
+          end
         end
       end
 
@@ -75,19 +87,35 @@ defmodule Mix.Tasks.Popcorn.Cook do
   defp gather_app_specs([], specs), do: specs
 
   defp gather_app_specs(apps, specs) do
+    if :logger in apps do
+      # Logger fails to start due to missing :persistent_term.get/0 implementation
+      raise "Application :logger not supported!"
+    end
+
+    if :popcorn in apps do
+      raise """
+        :popcorn application should not be part of the deps.
+        Ensure you have `runtime: false` set in deps/1 in your Mix.Project
+      """
+    end
+
     new_apps = Enum.reject(apps, &Map.has_key?(specs, &1))
 
     new_specs =
-      Map.new(new_apps, fn app ->
-        # Application.spec/1 doesn't return env, fetching it manually
+      for app <- new_apps,
+          spec = Application.spec(app),
+          # Ignore missing optional apps
+          spec != nil,
+          into: %{} do
         env = Application.get_all_env(app)
-        spec = app |> Application.spec() |> List.wrap()
         {app, [env: env] ++ spec}
-      end)
+      end
 
     deps =
       new_specs
-      |> Enum.flat_map(fn {_app, spec} -> spec[:applications] end)
+      |> Enum.flat_map(fn
+        {_app, spec} -> spec[:applications]
+      end)
       |> Enum.uniq()
 
     gather_app_specs(deps, Map.merge(specs, new_specs))
