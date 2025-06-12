b47937e338a99b2c8c25ce01d14c7e65deab3650
Mateusz Front <mateusz.front@swmansion.com>
2025-06-12 11:57:34 +0200

    format code (#177)

--- lib/popcorn/config.ex
+++ lib/popcorn/config.ex
@@ -6,8 +6,8 @@ defmodule Popcorn.Config do
       Path.wildcard("#{:code.lib_dir()}/{compiler,erts,kernel,stdlib}*/**/*.beam"),
     ex_stdlib_beam_paths:
       Path.wildcard("#{Application.app_dir(:elixir)}/ebin/**/*.beam") ++
-      Path.wildcard("#{Application.app_dir(:eex)}/ebin/**/*.beam") ++
-      Path.wildcard("#{Application.app_dir(:iex)}/ebin/**/*.beam"),
+        Path.wildcard("#{Application.app_dir(:eex)}/ebin/**/*.beam") ++
+        Path.wildcard("#{Application.app_dir(:iex)}/ebin/**/*.beam"),
     out_dir: nil,
     add_tracing: false,
     runtime_source: {:git, "git@github.com:software-mansion-labs/FissionVM.git"}
