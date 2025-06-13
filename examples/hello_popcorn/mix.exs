7ccb37d5c4793ea9ed9b3edd52cea9157a0f22be
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-06-13 12:29:20 +0200

    Improve README & hello_popcorn example

--- examples/hello_popcorn/mix.exs
+++ examples/hello_popcorn/mix.exs
@@ -14,7 +14,8 @@ defmodule HelloPopcorn.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: []
+      extra_applications: [],
+      mod: {HelloPopcorn.Application, []}
     ]
   end
 
--- lib/mix/tasks/popcorn.cook.ex
+++ lib/mix/tasks/popcorn.cook.ex
@@ -5,12 +5,11 @@ defmodule Mix.Tasks.Popcorn.Cook do
 
   Accepts the following options:
   - `out_dir` - the directory to write artifacts to
-  - `start_module` - a module with `start/0` function, the application's entry point
   - `target` - `wasm` (default) or `unix`. If `unix` is chosed, you need to build the runtime
   first with `mix popcorn.build_runtime --target unix`
 
-  `out_dir` and `start_module` are mandatory, unless provided
-  via `config.exs`, for example `config :popcorn, out_dir: "static/wasm, start_module: Start"`
+  `out_dir` is mandatory, unless provided via `config.exs`,
+  for example `config :popcorn, out_dir: "static/wasm"`
   """
   use Mix.Task
 
