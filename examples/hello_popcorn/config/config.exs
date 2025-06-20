1e7dcad490088e93abe58eee1aa1bbd665dd4166
Jakub Gonet <jakub.gonet@swmansion.com>
2025-06-20 16:42:43 +0200

    Add alias for building from source

--- examples/hello_popcorn/config/config.exs
+++ examples/hello_popcorn/config/config.exs
@@ -1,4 +1,6 @@
 import Config
 
 config :popcorn,
+  start_module: HelloPopcorn,
+  runtime_source: {:path, "../../../AtomVM"},
   out_dir: "static/wasm"
--- examples/hello_popcorn/mix.exs
+++ examples/hello_popcorn/mix.exs
@@ -7,7 +7,10 @@ defmodule HelloPopcorn.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: [
+        build_wasm: ["popcorn.build_runtime --target wasm", &copy_artefacts/1]
+      ]
     ]
   end
 
@@ -27,5 +30,16 @@ defmodule HelloPopcorn.MixProject do
       # playwright will be started manually
       {:playwright, github: "membraneframework-labs/playwright-elixir", runtime: false, only: :test}
     ]
+  end
+
+  defp copy_artefacts(_) do
+    mjs = Application.app_dir(:popcorn, ["atomvm_artifacts", "wasm", "AtomVM.mjs"])
+    wasm = Application.app_dir(:popcorn, ["atomvm_artifacts", "wasm", "AtomVM.wasm"])
+    popcorn_js = Application.app_dir(:popcorn, ["priv", "static-template", "wasm", "popcorn.js"])
+    popcorn_iframe_js = Application.app_dir(:popcorn, ["priv", "static-template", "wasm", "popcorn_iframe.js"])
+    File.cp!(mjs, "static/wasm/AtomVM.mjs")
+    File.cp!(wasm, "static/wasm/AtomVM.wasm")
+    File.cp!(popcorn_js, "static/wasm/popcorn.js")
+    File.cp!(popcorn_iframe_js, "static/wasm/popcorn_iframe.js")
   end
 end
