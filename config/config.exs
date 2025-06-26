2ad83f7f887dd0a687ef72269f0efc637d6d5dfb
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-06-26 11:03:24 +0200

    Testing the code_get_object

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,9 @@ import Config
 
 config :popcorn,
   add_tracing: false,
-  runtime_source: {:git, "git@github.com:software-mansion-labs/FissionVM.git"}
+  runtime_source:
+    {:git, "git@github.com:software-mansion-labs/FissionVM.git",
+     ref: "fkubis/exw-231/implement_code_get_object_code"}
 
 if File.exists?("#{__DIR__}/config.secret.exs") do
   # Put the following configuration in config.secret.exs:
