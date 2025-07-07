68144d4534f00685d26aadb4b54bc75609e20aad
Jakub Gonet <jakub.gonet@swmansion.com>
2025-07-07 14:23:01 +0200

    Remove custom branch from config

--- examples/game_of_life/config/config.exs
+++ examples/game_of_life/config/config.exs
@@ -4,7 +4,6 @@ root = "#{__DIR__}/../"
 
 config :popcorn,
   add_tracing: false,
-  runtime_source: {:git, "https://github.com/software-mansion-labs/FissionVM.git", ref: "jgonet/js-api-batch"},
   out_dir: "#{root}/static/wasm"
 
 if File.exists?("#{__DIR__}/config.secret.exs") do
--- examples/hello_popcorn/config/config.exs
+++ examples/hello_popcorn/config/config.exs
@@ -2,5 +2,4 @@ import Config
 
 config :popcorn,
   start_module: HelloPopcorn,
-  runtime_source: {:git, "https://github.com/software-mansion-labs/FissionVM.git", ref: "jgonet/js-api-batch"},
   out_dir: "static/wasm"
