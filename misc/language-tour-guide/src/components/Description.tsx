1a73fdeda29d2eb2b97182936dfffd7abd6a5116
marcel <marcel.mietla3@gmail.com>
2025-08-05 08:48:21 +0200

    Refactor Description and ExampleLayout components to use PropsWithChildren

--- misc/language-tour-guide/src/components/Description.tsx
+++ misc/language-tour-guide/src/components/Description.tsx
@@ -1,8 +1,6 @@
-interface DescriptionProps {
-  children: React.ReactNode;
-}
+import type { PropsWithChildren } from "react";
 
-export function Description({ children }: DescriptionProps) {
+export function Description({ children }: PropsWithChildren) {
   return (
     <section className="bg-light-30 border-grey-20 scrollbar h-full border p-4 wrap-break-word lg:row-span-2 lg:overflow-y-auto lg:p-8">
       {children}
--- misc/language-tour-guide/src/components/ExampleLayout.tsx
+++ misc/language-tour-guide/src/components/ExampleLayout.tsx
@@ -1,12 +1,9 @@
+import type { PropsWithChildren } from "react";
 import { CodeEditor } from "./CodeEditor/CodeEditor";
 import { Description } from "./Description";
 import { Results } from "./Results";
 
-interface ExampleLayoutProps {
-  children: React.ReactNode;
-}
-
-export function ExampleLayout({ children }: ExampleLayoutProps) {
+export function ExampleLayout({ children }: PropsWithChildren) {
   return (
     <div className="text-brown-100 gird-rows-[1fr_1fr_1fr] bg-light-20 grid h-full grow gap-x-4 gap-y-3 lg:min-h-0 lg:grid-cols-2 lg:grid-rows-[70%_28%] lg:gap-y-4">
       <Description>{children}</Description>
