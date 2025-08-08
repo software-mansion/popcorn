1156cb74e7167554e5a11b301d7747c2e4c9fab7
marcel <marcel.mietla3@gmail.com>
2025-08-08 08:42:31 +0200

    Add MDX support, and code block syntax highlighting

--- misc/language-tour-guide/src/components/Description.tsx
+++ misc/language-tour-guide/src/components/Description.tsx
@@ -1,9 +1,11 @@
+import { MDXProvider } from "@mdx-js/react";
 import type { PropsWithChildren } from "react";
+import { components } from "./markdown";
 
 export function Description({ children }: PropsWithChildren) {
   return (
     <section className="bg-light-30 border-grey-20 scrollbar h-full border p-4 wrap-break-word lg:row-span-2 lg:overflow-y-auto lg:p-8">
-      {children}
+      <MDXProvider components={components}>{children}</MDXProvider>
     </section>
   );
 }
--- misc/language-tour-guide/src/pages/Home.tsx
+++ misc/language-tour-guide/src/pages/Home.tsx
@@ -1,51 +1,15 @@
 import { ExampleLayout } from "../components/ExampleLayout";
+// import Agent from "../content/processes/Agent.mdx";
+
+import Introduction from "../content/getting-started/processes.mdx";
 
 export function Home() {
   return (
     <>
       <ExampleLayout>
-        <h1 className="font-handjet text-brown-70 mb-6 text-4xl font-bold">
-          Hello world
-        </h1>
-        <p className="font-inter text-brown-90 mb-4 text-lg">
-          This is an example layout for the Language Tour Guide. It includes a
-          header and a description section. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Cumque, eveniet autem odit nam nostrum
-          tempora quibusdam! Consectetur molestiae architecto nemo voluptate,
-          rem tempora, mollitia numquam, accusantium reprehenderit veniam amet
-          facere? This is an example layout for the Language Tour Guide. It
-          includes a header and a description section. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Cumque, eveniet autem odit nam
-          nostrum tempora quibusdam! Consectetur molestiae architecto nemo
-          voluptate, rem tempora, mollitia numquam, accusantium reprehenderit
-          veniam amet facere?
-        </p>
-        <p className="font-inter text-brown-90 mb-4 text-lg">
-          This is an example layout for the Language Tour Guide. It includes a
-          header and a description section. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Cumque, eveniet autem odit nam nostrum
-          tempora quibusdam! Consectetur molestiae architecto nemo voluptate,
-          rem tempora, mollitia numquam, accusantium reprehenderit veniam amet
-          facere? This is an example layout for the Language Tour Guide. It
-          includes a header and a description section. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Cumque, eveniet autem odit nam
-          nostrum tempora quibusdam! Consectetur molestiae architecto nemo
-          voluptate, rem tempora, mollitia numquam, accusantium reprehenderit
-          veniam amet facere?
-        </p>
-        <p className="font-inter text-brown-90 mb-4 text-lg">
-          This is an example layout for the Language Tour Guide. It includes a
-          header and a description section. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Cumque, eveniet autem odit nam nostrum
-          tempora quibusdam! Consectetur molestiae architecto nemo voluptate,
-          rem tempora, mollitia numquam, accusantium reprehenderit veniam amet
-          facere? This is an example layout for the Language Tour Guide. It
-          includes a header and a description section. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Cumque, eveniet autem odit nam
-          nostrum tempora quibusdam! Consectetur molestiae architecto nemo
-          voluptate, rem tempora, mollitia numquam, accusantium reprehenderit
-          veniam amet facere?
-        </p>
+        {/* <Agent /> */}
+
+        <Introduction />
       </ExampleLayout>
     </>
   );
