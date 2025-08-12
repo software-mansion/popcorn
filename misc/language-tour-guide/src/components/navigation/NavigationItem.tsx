9ee6733dba9b9bbbce5eb0e0e15f2c369bd4f699
marcel <marcel.mietla3@gmail.com>
2025-08-12 15:45:36 +0200

    Update NavigationItem to improve state handling and type definitions

--- misc/language-tour-guide/src/components/navigation/NavigationItem.tsx
+++ misc/language-tour-guide/src/components/navigation/NavigationItem.tsx
@@ -5,20 +5,23 @@ import ChevronDown from "../../assets/chevron-down.svg?react";
 import ChevronRight from "../../assets/chevron-right.svg?react";
 import { useState } from "react";
 
-export function NavigationItem({
-  item,
-  onClick
-}: {
+type NavigationItemProps = {
   item: NavigationTreeItem;
   onClick?: () => void;
-}) {
+};
+
+export function NavigationItem({ item, onClick }: NavigationItemProps) {
   const { hash, pathname } = useLocation();
 
   const [isOpen, setIsOpen] = useState(
-    `${pathname}${hash}`.includes(item.path || "")
+    `${pathname}${hash}`.includes(item.path ?? "")
   );
 
-  const hasChildren = item.children && Object.keys(item.children).length > 0;
+  const hasChildren = item.children !== undefined;
+
+  const isSelected = (item: NavigationTreeItem) => {
+    return item.path && item.path.includes(hash);
+  };
 
   return (
     <li className="my-1">
@@ -28,7 +31,7 @@ export function NavigationItem({
             ? "text-brown-header hover:bg-orange-20 mt-2 cursor-pointer font-semibold"
             : "text-brown-80"
         }`}
-        onClick={() => hasChildren && setIsOpen(!isOpen)}
+        onClick={() => hasChildren && setIsOpen((prev) => !prev)}
       >
         {hasChildren && (
           <span className="mr-2 text-xs text-orange-100">
@@ -44,7 +47,7 @@ export function NavigationItem({
             to={item.path}
             className={({ isActive }) =>
               `flex w-full items-center rounded-md px-3 py-0.5 text-sm transition-colors duration-200 ${
-                isActive && item.path && item.path.includes(hash)
+                isActive && isSelected(item)
                   ? "bg-color-orange-20 text-color-orange-100 font-semibold"
                   : "hover:bg-orange-20 text-color-brown-80 hover:text-color-brown-100"
               } ${hasChildren ? "pl-0" : "pl-5"}`
@@ -62,7 +65,7 @@ export function NavigationItem({
         <ul
           className={`pl-5 transition-all duration-300 ${isOpen ? "block opacity-100" : "hidden opacity-0"}`}
         >
-          {Object.entries(item.children || {}).map(([key, childItem]) => (
+          {Object.entries(item.children ?? {}).map(([key, childItem]) => (
             <NavigationItem key={`${key}`} item={childItem} onClick={onClick} />
           ))}
         </ul>
