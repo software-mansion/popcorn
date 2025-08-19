f7df4cb96a6b50800344d27990bdaf104db49faa
marcel <marcel.mietla3@gmail.com>
2025-08-19 14:05:51 +0200

    Refactor CodeBlock component; conditionally render "Try" button based on rawCode availability

--- misc/language-tour-guide/src/components/markdown/MarkdownComponents.tsx
+++ misc/language-tour-guide/src/components/markdown/MarkdownComponents.tsx
@@ -72,7 +72,7 @@ export const CodeBlock = ({ children, rawCode, ...props }: CodeBlockProps) => {
   const setCode = useCodeEditorStore((state) => state.setCode);
 
   const handleUseInEditor = useCallback(() => {
-    if (rawCode && setCode) {
+    if (rawCode) {
       setCode(rawCode);
     }
   }, [rawCode, setCode]);
@@ -83,21 +83,24 @@ export const CodeBlock = ({ children, rawCode, ...props }: CodeBlockProps) => {
       {...props}
     >
       {children}
-      <button
-        onClick={handleUseInEditor}
-        className="text-brown-90 absolute right-0 bottom-0 m-2 flex cursor-pointer items-center rounded bg-orange-100/80 px-2 py-1 text-xs transition-colors duration-200 hover:bg-orange-100"
-        aria-label="Use in editor"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="mr-1 h-3.5 w-3.5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+
+      {rawCode && (
+        <button
+          onClick={handleUseInEditor}
+          className="text-brown-90 absolute right-0 bottom-0 m-2 flex cursor-pointer items-center rounded bg-orange-100/80 px-2 py-1 text-xs transition-colors duration-200 hover:bg-orange-100"
+          aria-label="Use in editor"
         >
-          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-        </svg>
-        Try
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 h-3.5 w-3.5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+          </svg>
+          Try
+        </button>
+      )}
     </pre>
   );
 };
