c7a9cdd095c5e5c523f5693e2782495185e243f7
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-09-24 16:22:52 +0200

    Fix runtime source in tests

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,8 @@
 import Config
 
+config :popcorn,
+  runtime: [{:path, Path.join(__DIR__, "../popcorn_runtime_source/artifacts/$target")}]
+
 if File.exists?("#{__DIR__}/config.secret.exs") do
   # Put the following configuration in config.secret.exs:
   # config :popcorn, runtime_path: ...
