b4449a6a9f67559f3e884bb00da343b521471b4c
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-10-02 08:26:04 +0200

    Format

--- examples/local_live_view/lib/mix/tasks/compile/local_live_view.ex
+++ examples/local_live_view/lib/mix/tasks/compile/local_live_view.ex
@@ -6,7 +6,8 @@ defmodule Mix.Tasks.Compile.LocalLiveView do
   def run(_args) do
     deps_js_path =
       [Mix.Project.build_path(), "lib/local_live_view/priv/static/"]
-      |> Path.join
+      |> Path.join()
+
     dest_path = "static/assets"
     Mix.shell().cmd("mkdir -p #{dest_path}")
     Mix.shell().cmd("cp #{deps_js_path}/*.js #{dest_path}")
