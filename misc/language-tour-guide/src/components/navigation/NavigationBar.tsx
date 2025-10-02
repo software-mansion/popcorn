694f37db3069d951480d22b49e576068477b8fe7
Marceli Mietła <68269870+mmietla@users.noreply.github.com>
2025-10-02 15:18:10 +0200

    Improve chapter navigation (#322)
    Implements: #315 
    
    <img width="1992" height="1535" alt="image"
    src="https://github.com/user-attachments/assets/61e85105-a431-4441-a400-58bd58872f20"
    />
    
    <img width="1990" height="1537" alt="image"
    src="https://github.com/user-attachments/assets/295d2844-71c3-48d0-84e1-65a64ad9df6b"
    />
    
    <img width="376" height="670" alt="image"
    src="https://github.com/user-attachments/assets/ba0da96f-9036-4b2a-95ef-a5fae937a5f0"
    />
    
    <img width="378" height="670" alt="image"
    src="https://github.com/user-attachments/assets/22b2ed5d-c374-467f-95f3-db2ebedb77ea"
    />

--- misc/language-tour-guide/src/components/navigation/NavigationBar.tsx
+++ misc/language-tour-guide/src/components/navigation/NavigationBar.tsx
@@ -4,14 +4,27 @@ import { useSiblingsNode } from "../../utils/hooks/useSiblingsNode";
 export default function NavigationBar() {
   const { siblingsNode, childrenCount, currentIndex } = useSiblingsNode();
 
+  const isAtFirstInChapter = currentIndex === 1 || childrenCount === 0;
+  const isAtLastInChapter = currentIndex === childrenCount;
+  const isTopLevelChapter = childrenCount !== 0 || currentIndex !== 0;
+
   return (
     <div className="bg-light-20 fixed bottom-0 left-0 z-10 flex w-full items-center justify-center gap-7 border-t border-t-orange-100 p-2 lg:sticky lg:p-3">
       <NavigationButton
         direction="previous"
         path={siblingsNode?.previousNode?.path}
+        isChapterNavigation={isAtFirstInChapter}
       />
-      {currentIndex}/{childrenCount}
-      <NavigationButton direction="next" path={siblingsNode?.nextNode?.path} />
+      {isTopLevelChapter && (
+        <span>
+          {currentIndex}/{childrenCount}
+        </span>
+      )}
+      <NavigationButton
+        direction="next"
+        path={siblingsNode?.nextNode?.path}
+        isChapterNavigation={isAtLastInChapter}
+      />
     </div>
   );
 }
--- misc/language-tour-guide/src/components/navigation/NavigationButton.tsx
+++ misc/language-tour-guide/src/components/navigation/NavigationButton.tsx
@@ -6,24 +6,46 @@ type Direction = "next" | "previous";
 type NavigationButtonProps = {
   direction: Direction;
   path?: string;
+  isChapterNavigation?: boolean;
 };
 
-export function NavigationButton({ direction, path }: NavigationButtonProps) {
+export function NavigationButton({
+  direction,
+  path,
+  isChapterNavigation
+}: NavigationButtonProps) {
   const Icon = direction === "next" ? ChevronRight : ChevronLeft;
   const label = direction === "next" ? "Next section" : "Previous section";
+  const chapterLabel =
+    direction === "next" ? "Next chapter" : "Previous chapter";
 
-  if (path) {
+  if (!path) {
     return (
-      <NavLink to={path}>
-        <span className="sr-only">{label}</span>
-        <Icon className="orange-shadow w-8 rounded-full bg-orange-100 p-1 text-white transition-all duration-300 hover:bg-orange-200 lg:w-9" />
+      <button disabled className="group cursor-not-allowed" aria-hidden="true">
+        <Icon className="border-brown-60 group-disabled:bg-grey-20 group-disabled:text-brown-60 w-8 rounded-full border p-1 shadow-md shadow-black/10 lg:w-9" />
+      </button>
+    );
+  }
+
+  if (isChapterNavigation) {
+    return (
+      <NavLink
+        to={path}
+        className="transition-color flex items-center gap-1 rounded-full bg-orange-100 text-sm font-medium text-white duration-300 hover:bg-orange-200"
+      >
+        {direction === "previous" && <Icon className="w-8 p-1 lg:w-9" />}
+        <span className={direction === "next" ? "pl-4" : "pr-4"}>
+          {chapterLabel}
+        </span>
+        {direction === "next" && <Icon className="w-8 p-1 lg:w-9" />}
       </NavLink>
     );
   }
 
   return (
-    <button disabled className="group cursor-not-allowed" aria-hidden="true">
-      <Icon className="border-brown-60 group-disabled:bg-grey-20 group-disabled:text-brown-60 w-8 rounded-full border p-1 shadow-md shadow-black/10 lg:w-9" />
-    </button>
+    <NavLink to={path}>
+      <span className="sr-only">{label}</span>
+      <Icon className="orange-shadow w-8 rounded-full bg-orange-100 p-1 text-white transition-all duration-300 hover:bg-orange-200 lg:w-9" />
+    </NavLink>
   );
 }
