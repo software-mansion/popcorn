0a2912459607f846f593ed6c6dc9bfbd3be3d36f
marcel <marcel.mietla3@gmail.com>
2025-10-22 18:26:44 +0200

    Make bigger font size of output and fix not wrapping text

--- misc/language-tour-guide/src/components/execution/StdoutResults.tsx
+++ misc/language-tour-guide/src/components/execution/StdoutResults.tsx
@@ -5,12 +5,12 @@ type StdoutResultsProps = {
 export default function StdoutResults({ stdout }: StdoutResultsProps) {
   return (
     <div className="mt-2 rounded border-l-2 border-blue-400 bg-gray-50 p-2 text-sm whitespace-pre-wrap">
-      <div className="mb-1 flex flex-col gap-1">
-        <span className="text-grey-70 text-xs font-medium">Output:</span>
+      <div className="mb-1 flex flex-col gap-1.5 text-sm font-medium">
+        <span className="text-grey-70">Output:</span>
         {stdout.map((line, index) => (
           <span
             key={`stdout-${index}-${line}`}
-            className="text-brown-90/70 font-mono text-xs font-medium"
+            className="text-brown-90/70 font-mono break-words"
           >
             {line}
           </span>
