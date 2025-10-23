9568192bff1d80fb1605995716a40548961dc601
marcel <marcel.mietla3@gmail.com>
2025-10-23 13:44:18 +0200

    Remove result info from introduction.mdx and remove left border from output

--- misc/language-tour-guide/src/components/execution/StdoutResults.tsx
+++ misc/language-tour-guide/src/components/execution/StdoutResults.tsx
@@ -4,9 +4,9 @@ type StdoutResultsProps = {
 
 export default function StdoutResults({ stdout }: StdoutResultsProps) {
   return (
-    <div className="mt-2 rounded border-l-2 border-blue-400 bg-gray-50 p-2 text-sm whitespace-pre-wrap">
-      <div className="mb-1 flex flex-col gap-1.5 text-sm font-medium">
-        <span className="text-grey-70">Output:</span>
+    <div className="mt-2 rounded bg-gray-50 p-2 text-sm whitespace-pre-wrap">
+      <div className="mb-1 flex flex-col gap-1 text-sm font-medium">
+        <span className="text-grey-70 mb-2">Output:</span>
         {stdout.map((line, index) => (
           <span
             key={`stdout-${index}-${line}`}
