8c49361d1db5d3194ece114292f83ef7057a0041
marcel <marcel.mietla3@gmail.com>
2025-10-27 08:39:49 +0100

    Added scroll-to-top behavior on page change and implemented per-page execution history

--- misc/language-tour-guide/src/utils/hooks/useOnNavigationChange.ts
+++ misc/language-tour-guide/src/utils/hooks/useOnNavigationChange.ts
@@ -1,15 +1,23 @@
 import { useLocation } from "react-router";
 import { useCodeEditorStore } from "../../store/codeEditor";
 import { useEffect } from "react";
+import { useExecutionHistoryStore } from "../../store/executionHistory";
 
 export function useOnNavigationChange(callback: () => void) {
   const { pathname } = useLocation();
   const resetStdoutResult = useCodeEditorStore(
     (state) => state.resetStdoutResult
   );
+  const resetStderrResult = useCodeEditorStore(
+    (state) => state.resetStderrResult
+  );
+  const clearHistory = useExecutionHistoryStore((state) => state.clearHistory);
 
   useEffect(() => {
     resetStdoutResult();
+    resetStderrResult();
+    clearHistory();
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
     callback();
-  }, [pathname, callback, resetStdoutResult]);
+  }, [pathname, callback, resetStdoutResult, resetStderrResult, clearHistory]);
 }
