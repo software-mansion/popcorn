1dca710966f21ff820cc833594908e83e3713b3d
Marceli Mietła <68269870+mmietla@users.noreply.github.com>
2025-10-30 10:35:55 +0100

    [Lang tour] feedback changes (#372)
    Implements: #368 
    
    
    
    https://github.com/user-attachments/assets/f689e165-27d1-4e2f-bca6-b6cebbb4aabd
    
    
    
    https://github.com/user-attachments/assets/65432b47-f84c-4309-8c44-caf36f99129a

--- misc/language-tour-guide/src/utils/hooks/useOnNavigationChange.ts
+++ misc/language-tour-guide/src/utils/hooks/useOnNavigationChange.ts
@@ -1,15 +1,29 @@
 import { useLocation } from "react-router";
 import { useCodeEditorStore } from "../../store/codeEditor";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useExecutionHistoryStore } from "../../store/executionHistory";
 
 export function useOnNavigationChange(callback: () => void) {
   const { pathname } = useLocation();
   const resetStdoutResult = useCodeEditorStore(
     (state) => state.resetStdoutResult
   );
+  const resetStderrResult = useCodeEditorStore(
+    (state) => state.resetStderrResult
+  );
+  const clearHistory = useExecutionHistoryStore((state) => state.clearHistory);
+
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
 
   useEffect(() => {
     resetStdoutResult();
-    callback();
-  }, [pathname, callback, resetStdoutResult]);
+    resetStderrResult();
+    clearHistory();
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    callbackRef.current();
+  }, [pathname, resetStdoutResult, resetStderrResult, clearHistory]);
 }
