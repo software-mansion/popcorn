24056eade32620749bc07fbb93694b53639fd5ad
marcel <marcel.mietla3@gmail.com>
2025-11-04 15:04:22 +0100

    Refactor PopcornLoader to display conditionally with the Result and History components

--- misc/language-tour-guide/src/components/execution/Execution.tsx
+++ misc/language-tour-guide/src/components/execution/Execution.tsx
@@ -1,13 +1,21 @@
+import { usePopcorn } from "../../context/popcorn/actions";
 import { History } from "./History";
 import { PopcornLoader } from "./PopcornLoader";
 import { Results } from "./Results";
 
 export default function Execution() {
+  const { isLoadingPopcorn } = usePopcorn();
+
   return (
     <section className="bg-light-30 border-grey-20 scrollbar relative flex h-full min-h-100 flex-col gap-4 overflow-y-auto rounded-md border pb-28 lg:min-h-60 lg:pb-4">
-      <PopcornLoader />
-      <Results />
-      <History />
+      {isLoadingPopcorn ? (
+        <PopcornLoader />
+      ) : (
+        <>
+          <Results />
+          <History />
+        </>
+      )}
     </section>
   );
 }
