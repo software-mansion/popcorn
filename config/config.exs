311c2c0a5019a859eb319008b9eee0d7c974149f
Mateusz Front <mateusz.front@swmansion.com>
2025-11-06 11:36:14 +0100

    Cover bugs fixed by downstreamed AtomVM in tests

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,5 @@
 import Config
 
 if File.exists?("#{__DIR__}/config.secret.exs") do
-  # Put the following configuration in config.secret.exs:
-  # config :popcorn, runtime_path: ...
   import_config "config.secret.exs"
 end
--- examples/hello_popcorn/config/config.exs
+++ examples/hello_popcorn/config/config.exs
@@ -2,3 +2,7 @@ import Config
 
 config :popcorn,
   out_dir: "static/wasm"
+
+if File.exists?("#{__DIR__}/config.secret.exs") do
+  import_config "config.secret.exs"
+end
