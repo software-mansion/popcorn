0aacff5936cee86a3ea1f0fc7d9075866370ab63
Jakub Gonet <jakub.gonet@swmansion.com>
2025-11-06 16:05:02 +0100

    Add single-line logs when building bundles (#329)
    Prints all operations in the same line (in interactive terminals) which
    limits log spam.
    
    ## Screencast
    
    Click to play.
    
    
    [![asciicast](https://asciinema.org/a/BfXBCK37hOpT0ncDukTpt4za8.svg)](https://asciinema.org/a/BfXBCK37hOpT0ncDukTpt4za8)

--- lib/popcorn/build.ex
+++ lib/popcorn/build.ex
@@ -1,7 +1,9 @@
 defmodule Popcorn.Build do
   @moduledoc false
   require Popcorn.Config
+  require Logger
   alias Popcorn.CoreErlangUtils
+  alias Popcorn.BuildLogFormatter
 
   @config Popcorn.Config.compile([:add_tracing, :extra_apps])
 
@@ -33,6 +35,10 @@ defmodule Popcorn.Build do
   with all additional BEAMs
   """
   def build(opts \\ []) do
+    Logger.info("Bundling started")
+    start_time = DateTime.utc_now()
+    original_formatter = BuildLogFormatter.enable()
+
     opts =
       opts
       |> Keyword.validate!(force: false)
@@ -81,6 +87,10 @@ defmodule Popcorn.Build do
     File.write!(@cache_path, :erlang.term_to_binary(new_cache))
 
     :ok
+    BuildLogFormatter.disable(original_formatter)
+    end_time = DateTime.utc_now()
+    duration = DateTime.diff(end_time, start_time, :microsecond)
+    Logger.info("Bundling finished (took #{to_human_duration(duration)})")
   end
 
   @doc """
@@ -142,12 +152,12 @@ defmodule Popcorn.Build do
 
     {modified_srcs, cache} = update_cache(patches_srcs, cache)
 
-    patched_beams = patch(app_beams, modified_srcs, build_dir)
+    patched_beams = patch(application, app_beams, modified_srcs, build_dir)
 
-    transferred_beams = transfer_stdlib(app_beams, build_dir)
+    transferred_beams = transfer_stdlib(application, app_beams, build_dir)
 
     if patched_beams != [] or transferred_beams != [] do
-      IO.puts(:stderr, "Bundling #{application}.avm")
+      Logger.info("Bundling #{application}.avm", app_name: application)
       :packbeam_api.create(to_charlist(bundle_path(application)), bundle_beams(application))
     end
 
@@ -162,10 +172,10 @@ defmodule Popcorn.Build do
 
     {modified_srcs, cache} = update_cache(srcs, cache)
 
-    popcorn_lib_beams = patch([], modified_srcs, build_dir)
+    popcorn_lib_beams = patch(bundle, [], modified_srcs, build_dir)
 
     if popcorn_lib_beams != [] do
-      IO.puts(:stderr, "Bundling #{bundle}.avm")
+      Logger.info("Bundling #{bundle}.avm", app_name: bundle)
       :packbeam_api.create(to_charlist(bundle_path(bundle)), bundle_beams(bundle))
     end
 
@@ -191,7 +201,7 @@ defmodule Popcorn.Build do
   # The patching works by replacing original functions implementations
   # with custom ones.
   # The sources of the patches reside in the `patches` directory.
-  defp patch(stdlib_beams, patch_srcs, out_dir) do
+  defp patch(app, stdlib_beams, patch_srcs, out_dir) do
     stdlib_beams_by_name = Map.new(stdlib_beams, &{Path.basename(&1), &1})
 
     # Compiling each file separately, like AtomVM does it.
@@ -201,13 +211,13 @@ defmodule Popcorn.Build do
       tmp_dir = setup_tmp_dir(out_dir)
 
       try do
-        IO.puts(:stderr, "Compiling #{src}")
+        Logger.info("Compiling #{src}", app_name: app)
         compile_patch(src, tmp_dir)
 
         Path.wildcard("#{tmp_dir}/*")
         |> Enum.map(fn path ->
           name = Path.basename(path)
-          do_patch(name, stdlib_beams_by_name[name], path, out_dir)
+          do_patch(app, name, stdlib_beams_by_name[name], path, out_dir)
           name
         end)
       after
@@ -238,23 +248,25 @@ defmodule Popcorn.Build do
 
   # prim_eval fails to be parsed, probably because it's compiled from an asm (*.S) file
   # so we just copy it
-  defp do_patch("prim_eval.beam" = name, _stdlib, patch, out_dir) do
+  defp do_patch(app_name, "prim_eval.beam" = name, _stdlib, patch, out_dir) do
+    Logger.info("Patching #{name}", app_name: app_name)
+
     File.cp!(patch, Path.join(out_dir, name))
   end
 
   # This is only needed to add tracing
   # but we execute it always for consistency
   # To be replaced with File.cp when we improve tracing in AtomVM
-  defp do_patch(name, nil, patch, out_dir) do
-    IO.puts(:stderr, "Patching #{name}")
+  defp do_patch(app_name, name, nil, patch, out_dir) do
+    Logger.info("Patching #{name}", app_name: app_name)
     ast = File.read!(patch) |> CoreErlangUtils.parse()
     ast = if @config.add_tracing, do: CoreErlangUtils.add_simple_tracing(ast), else: ast
     beam = CoreErlangUtils.serialize(ast)
     File.write!(Path.join(out_dir, name), beam)
   end
 
-  defp do_patch(name, stdlib, patch, out_dir) do
-    IO.puts(:stderr, "Patching #{name}")
+  defp do_patch(app_name, name, stdlib, patch, out_dir) do
+    Logger.info("Patching #{name}", app_name: app_name)
 
     ast =
       CoreErlangUtils.merge_modules(
@@ -270,14 +282,14 @@ defmodule Popcorn.Build do
   # This is only needed to add tracing
   # but we execute it always for consistency
   # To be removed when we improve tracing in AtomVM
-  defp transfer_stdlib(stdlib_beams, out_dir) do
+  defp transfer_stdlib(app_name, stdlib_beams, out_dir) do
     already_transferred = MapSet.new(File.ls!(out_dir))
 
     stdlib_beams
     |> Enum.reject(&(Path.basename(&1) in already_transferred))
     |> process_async(fn path ->
       name = Path.basename(path)
-      IO.puts(:stderr, "Transferring #{name}")
+      Logger.info("Transferring #{name}", app_name: app_name)
       ast = File.read!(path) |> CoreErlangUtils.parse()
       ast = if @config.add_tracing, do: CoreErlangUtils.add_simple_tracing(ast), else: ast
       beam = CoreErlangUtils.serialize(ast)
@@ -297,5 +309,17 @@ defmodule Popcorn.Build do
     File.rm_rf!(tmp_dir)
     File.mkdir!(tmp_dir)
     tmp_dir
+  end
+
+  defp to_human_duration(us) do
+    us = :erlang.float(us)
+    ms = us / 1_000
+    s = ms / 1_000
+
+    cond do
+      s > 1.0 -> "#{Float.round(s, 2)}s"
+      ms > 1.0 -> "#{Float.round(ms, 2)}ms"
+      true -> "#{Float.round(us, 2)}us"
+    end
   end
 end
