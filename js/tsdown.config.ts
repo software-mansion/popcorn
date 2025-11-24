99786abbe80f270991dfa99148cb1c7212f54604
Jakub Gonet <jakub.gonet@swmansion.com>
2025-11-24 16:47:40 +0100

    Pack JS after build

--- js/tsdown.config.ts
+++ js/tsdown.config.ts
@@ -1,5 +1,18 @@
-import { defineConfig } from "tsdown";
+import { defineConfig, UserConfig } from "tsdown";
+import util from "util";
+import { exec as execRaw } from "child_process";
+const exec = util.promisify(execRaw);
+
+const rebuildPackage: UserConfig = {
+  ignoreWatch: [/swmansion-popcorn.+.tgz/],
+  hooks(hooks) {
+    hooks.hook("build:done", () => {
+      exec("npm pack");
+    });
+  },
+};
 
 export default defineConfig({
   exports: true,
+  ...rebuildPackage,
 });
