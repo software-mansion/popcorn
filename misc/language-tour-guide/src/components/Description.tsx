50f163db2cf93e66879ca666c0a1c374dee41e47
Marceli Mietła <marceli.mietla@swmansion.com>
2025-11-28 14:45:51 +0100

    Langtour: single page layout (#426)
    Implements: #383 
    
    <img width="1397" height="928" alt="image"
    src="https://github.com/user-attachments/assets/0d1600f3-12ad-4c18-b482-d8c8f80e850d"
    />
    <img width="382" height="827" alt="image"
    src="https://github.com/user-attachments/assets/bdb2316e-a07c-447e-915d-7eedbee57345"
    />

--- misc/language-tour-guide/src/components/Description.tsx
+++ misc/language-tour-guide/src/components/Description.tsx
@@ -1,16 +1,26 @@
 import { MDXProvider } from "@mdx-js/react";
-import type { PropsWithChildren } from "react";
 import { components } from "./markdown";
 import NavigationBar from "./navigation/NavigationBar";
+import { usePopcorn } from "../utils/hooks/usePopcorn";
+import { Loader } from "./Loader";
+import { Outlet } from "react-router";
 
-export function Description({ children }: PropsWithChildren) {
+export function Description() {
+  const { isLoadingPopcorn } = usePopcorn();
+
   return (
     <section
       id="description"
-      className="bg-light-30 border-grey-20 scrollbar relative flex h-full w-full flex-col border lg:row-span-2 lg:overflow-y-auto"
+      className="bg-light-20 text-brown-100 scrollbar relative flex h-full w-full grow flex-col"
     >
-      <div className="grow p-4 wrap-break-word lg:p-8">
-        <MDXProvider components={components}>{children}</MDXProvider>
+      <div className="flex max-w-[1024px] grow flex-col p-8 pt-8 wrap-break-word lg:mx-auto lg:px-16">
+        {isLoadingPopcorn ? (
+          <Loader message="Popcorn is loading..." />
+        ) : (
+          <MDXProvider components={components}>
+            <Outlet />
+          </MDXProvider>
+        )}
       </div>
       <NavigationBar />
     </section>
