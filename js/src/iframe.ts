a9ec16127d0ab58bec40a7a8e58d0eaabe4646a6
Jakub Gonet <jakub.gonet@swmansion.com>
2025-12-01 13:00:42 +0100

    Build AtomVM

--- js/src/iframe.ts
+++ js/src/iframe.ts
@@ -1,4 +1,7 @@
+import init from "../assets/AtomVM.mjs";
+
 export function main(this: Window) {
+  init();
   console.log("Hello from iframe", {
     w: this.window,
     isIframe: this.window.self !== this.window.top,
--- js/tsdown.config.ts
+++ js/tsdown.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig, UserConfig } from "tsdown";
 import util from "util";
 import { exec as execRaw } from "child_process";
+import copy from "rollup-plugin-copy";
 const exec = util.promisify(execRaw);
-
-const rebuildPackage: UserConfig = {
-  ignoreWatch: [/swmansion-popcorn.+.tgz/],
-  hooks(hooks) {
-    hooks.hook("build:done", () => {
-      exec("npm pack");
-    });
-  },
-};
 
 export default defineConfig({
   exports: true,
-  ...rebuildPackage,
+  plugins: [
+    copy({ targets: [{ src: "./assets/AtomVM.wasm", dest: "dist/" }] }),
+  ],
+  ...rebuildPackage(),
 });
+
+function rebuildPackage(): UserConfig {
+  return {
+    ignoreWatch: [/swmansion-popcorn.+.tgz/],
+    hooks(hooks) {
+      hooks.hook("build:done", () => {
+        exec("npm pack");
+      });
+    },
+  };
+}
