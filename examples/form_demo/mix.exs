163d51e2b43b24aa63dd95c0e0949e3eee7162ba
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-12-02 16:01:31 +0100

    Fix setup error and added form_demo to CI.

--- examples/form_demo/mix.exs
+++ examples/form_demo/mix.exs
@@ -74,7 +74,7 @@ defmodule FormDemo.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "assets.setup", "assets.build", &build_local/1],
+      setup: ["deps.get", "compile", "assets.setup", "assets.build", &build_local/1],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind form_demo", "esbuild form_demo"],
       "assets.deploy": [
