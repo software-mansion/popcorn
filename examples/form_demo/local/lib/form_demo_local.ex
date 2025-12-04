0c1c32cdb61db88d58187d9944ba46a0f67fd7d2
Franciszek Kubis <franciszek.kubis@swmansion.com>
2025-12-04 14:00:02 +0100

    Changed email input type to text because of non-present selection_range attribute browser bug

--- examples/form_demo/local/lib/form_demo_local.ex
+++ examples/form_demo/local/lib/form_demo_local.ex
@@ -9,12 +9,14 @@ defmodule FormDemoLocal do
       <label>USERNAME</label>
       <.input type="text" field={@form[:username]} />
       <label>EMAIL</label>
-      <.input type="email" field={@form[:email]} />
+      <.input type="text" field={@form[:email]} />
       <div class="centered">
         <button class="ghost-button" disabled={@disabled}>SAVE</button>
       </div>
     </.form>
-    <p style="color:red;"><%= @errors %></p>
+    <%= for error <- @errors do %>
+      <p style="color:red;"><%= error %></p>
+    <% end %>
     <div class="bordered">
       <h1>User List:</h1>
       <ul>
@@ -34,33 +36,62 @@ defmodule FormDemoLocal do
       %{"email" => "user3@example.com", "username" => "user3"}
     ]
     user = %{"email" => "", "username" => ""}
-    {:ok, assign(socket, users: users, form: to_form(user), errors: "", disabled: false)}
+    {:ok, assign(socket, users: users, form: to_form(user), errors: [], disabled: true)}
   end
 
   @impl true
   def handle_event("validate", params, socket) do
     errors = validate(params, socket.assigns.users)
-    {:noreply, assign(socket, form: to_form(params), errors: errors, disabled: errors != "")}
+    IO.inspect(errors)
+    {:noreply, assign(socket, form: to_form(params), errors: errors, disabled: errors != [])}
   end
 
   def handle_event("save", user_params, socket) do
     users = socket.assigns.users
     case validate(user_params, users) do
-      "" ->
-        {:noreply, assign(socket, users: users ++ [user_params], errors: "")}
+      [] ->
+        user = %{"email" => "", "username" => ""}
+        {:noreply, assign(socket, form: to_form(user), users: users ++ [user_params], errors: [], disabled: true)}
 
       errors ->
-        {:noreply, assign(socket, errors: errors)}
+        {:noreply, assign(socket, errors: errors, disabled: true)}
     end
   end
 
   defp validate(user, existing_users) do
+    validate_correctness(user) ++ validate_already_existing(user, existing_users)
+    |> Enum.filter(fn error -> error != "" end)
+  end
+  
+  defp validate_already_existing(user, existing_users) do
     user
     |> Enum.filter(fn {key, value} ->
       Enum.any?(existing_users, fn user -> Map.get(user, key) == value end)
     end)
-    |> Enum.map_join(", ", fn {key, _value} ->
+    |> Enum.map(fn {key, _value} ->
       String.capitalize("#{key} already in use")
-      end)
+    end)
+  end
+  
+  defp validate_correctness(user) do
+    Enum.map(user, fn {key, value} -> validate_correctness(key, value) end)
+  end
+
+  defp validate_correctness("username", value) do
+    cond do
+      String.length(value) < 4 -> "Username length must be greater than 3 characters"
+      true -> ""
+    end
+  end
+  
+  defp validate_correctness("email", value) do
+    case String.split(value, "@") do
+      [name, server] ->
+        case String.length(name) > 0 and String.contains?(server, ".") do
+          true -> ""
+          false -> "Email must have an email format"
+        end
+      _ -> "Email must have an email format"
+    end
   end
 end
