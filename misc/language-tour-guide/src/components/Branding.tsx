53662d9127c9d114b01116f9d499c0dc987b8ec4
Marceli Mietła <marceli.mietla@swmansion.com>
2026-01-22 09:34:34 +0100

    Update Branding and Navigation components; add hire us text and adjust styles

--- misc/language-tour-guide/src/components/Branding.tsx
+++ misc/language-tour-guide/src/components/Branding.tsx
@@ -4,15 +4,25 @@ import Logo from "../assets/logo.svg?react";
 
 export function Branding() {
   return (
-    <nav className="text-brown-80 bg-light-20 top-0 z-30 flex h-16 w-full items-center gap-6 border-b border-orange-100 px-4 py-4 lg:px-8">
+    <nav className="text-brown-80 bg-light-20 top-0 z-30 flex h-16 w-full items-center gap-2 border-b border-orange-100 px-4 py-4 sm:gap-6 lg:px-8">
       <Link to="https://popcorn.swmansion.com/" target="_blank">
         <Logo className="h-9 w-max md:hidden" />
         <LogoText className="hidden h-9 w-max md:block" />
         <p className="sr-only">popcorn</p>
       </Link>
-      <Link to="/" className="font-handjet text-2xl lg:text-3xl">
+      <Link to="/" className="font-handjet text-xl sm:text-3xl">
         Elixir Language Tour
       </Link>
+      <div className="text-brown-70 text-2xs ml-auto flex flex-col text-right sm:text-sm">
+        <span>Created by Software Mansion</span>
+        <Link
+          to="https://swmansion.com/contact"
+          target="_blank"
+          className="font-medium text-orange-200 underline transition-colors hover:text-orange-100"
+        >
+          Hire us
+        </Link>
+      </div>
     </nav>
   );
 }
