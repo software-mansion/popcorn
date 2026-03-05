dd714cb2f0c5e37e243499335d7c42b47967daf6
Jakub Gonet <jakub.gonet@swmansion.com>
2026-03-06 00:24:22 +0100

    example: use js package for hello-popcorn

--- examples/hello-popcorn/assets/build.mjs
+++ examples/hello-popcorn/assets/build.mjs
@@ -1,11 +1,15 @@
 import * as esbuild from "esbuild";
 import { popcorn } from "@swmansion/popcorn/esbuild";
+import { copyFile, mkdir } from "fs/promises";
+
+await mkdir("../dist", { recursive: true });
+await copyFile("index.html", "../dist/index.html");
 
 await esbuild.build({
   entryPoints: ["index.js"],
   bundle: true,
   format: "esm",
   sourcemap: true,
-  outfile: "out/index.js",
-  plugins: [popcorn({ bundlePath: "../_build/wasm/bundle.avm" })],
+  outfile: "../dist/index.js",
+  plugins: [popcorn({ bundlePath: "../dist/wasm/bundle.avm" })],
 });
