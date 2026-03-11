cc3cdf7b4d0d4e0b22625a43255e49905a8c0a0a
Jakub Gonet <jakub.gonet@swmansion.com>
2026-03-11 14:01:17 +0100

    examples: unify tasks in hello-react

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -198,6 +198,12 @@ main() {
          cd "${example_dir}"
          mix dev
 
+    elif [[ "${EXAMPLE}" == "hello-react" ]]; then
+         local example_dir="${PROJECT_ROOT}/examples/${EXAMPLE}"
+         log "Setting up example: ${EXAMPLE}"
+         cd "${example_dir}"
+         pnpm run dev
+
     elif [[ -n "${EXAMPLE}" ]]; then
         ensure_wasm_assets "${js_dir}"
         ensure_js_dist "${js_dir}"
