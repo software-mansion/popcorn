4fba2b3effe38b25c449f72d88a01859a405ebb2
Jakub Gonet <jakub.gonet@swmansion.com>
2026-03-11 14:03:29 +0100

    examples: unify tasks in iex-wasm

--- examples/iex-wasm/mix.exs
+++ examples/iex-wasm/mix.exs
@@ -10,7 +10,8 @@ defmodule IexWasm.MixProject do
       compilers: Mix.compilers(),
       deps: deps(),
       aliases: [
-        build_wasm: "popcorn.build_runtime --target wasm"
+        build: ["deps.get", "popcorn.build_runtime --target wasm", "popcorn.cook --include-vm"],
+        dev: ["build", "popcorn.server"]
       ]
     ]
   end
