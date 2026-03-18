27239a4951be4c0387328593c8c3a8cce56c11d3
Mateusz Front <mateusz.front@swmansion.com>
2026-03-18 15:13:05 +0100

    add patch docs

--- popcorn/elixir/patches/elixir/elixir/dynamic_supervisor.ex
+++ popcorn/elixir/patches/elixir/elixir/dynamic_supervisor.ex
@@ -1,5 +1,7 @@
 defmodule DynamicSupervisor do
   @compile {:popcorn_patch_private, add_restart: 1}
+
+  # Patch reason: :erlang.monotonic_time(1) -> :erlang.monotonic_time(:second)
   def add_restart(state) do
     %{max_seconds: max_seconds, max_restarts: max_restarts, restarts: restarts} = state
 
