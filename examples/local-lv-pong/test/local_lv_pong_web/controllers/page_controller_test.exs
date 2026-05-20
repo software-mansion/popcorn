c89412e71e84575d22a1cb81b53663411312fd52
Mateusz Front <mateusz.front@swmansion.com>
2026-05-20 16:21:26 +0200

    Create local-lv-pong project

---
+++ examples/local-lv-pong/test/local_lv_pong_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule LocalLvPongWeb.PageControllerTest do
+  use LocalLvPongWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
