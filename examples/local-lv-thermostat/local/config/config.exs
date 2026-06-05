76f13ccfee4435e59cd13f58e0e71e969e1db70d
Marceli Mietła <marceli.mietla@swmansion.com>
2026-06-05 09:11:00 +0200

    Show terminal loading state in LiveDemo

--- examples/local-lv-thermostat/local/config/config.exs
+++ examples/local-lv-thermostat/local/config/config.exs
@@ -3,4 +3,4 @@ import Config
 config :popcorn,
   out_dir: "../priv/static/assets/js/wasm"
 
-config :popcorn, treeshake: System.get_env("POPCORN_TREESHAKE") |> String.downcase() == "true"
+config :popcorn, treeshake: System.get_env("POPCORN_TREESHAKE", "false") |> String.downcase() == "true"
