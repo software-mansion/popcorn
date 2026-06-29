7f2fab1e7ccdc83f74e6b9a381407ec40147e61f
Franciszek Kubis <franciszek.kubis@swmansion.com>
2026-06-29 14:49:57 +0200

    LLV thermostat tests

--- examples/local-lv-thermostat/test/local_thermostat_web/controllers/page_controller_test.exs
+++ examples/local-lv-thermostat/test/local_thermostat_web/controllers/page_controller_test.exs
@@ -1,8 +1,12 @@
 defmodule LocalThermostatWeb.PageControllerTest do
   use LocalThermostatWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / renders the LocalLiveView mount points", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    html = html_response(conn, 200)
+
+    # The server renders mount points; the views themselves boot in the browser via AtomVM.
+    assert html =~ ~s(data-pop-view="ThermostatLive")
+    assert html =~ ~s(data-pop-view="ClockLive")
   end
 end
