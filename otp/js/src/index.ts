6b5c0a66ede02ac4d1b68350a3a0406fc6a00643
Jakub Gonet <jakub.gonet@swmansion.com>
2026-07-13 22:30:27 +0200

    otp: worker as separate entrypoint (#671)
    This was needed to avoid listing files in vite plugin. This should
    produce two entrypoints, one for main context, one for webworker
    context.

--- otp/js/src/index.ts
+++ otp/js/src/index.ts
@@ -1,12 +1,6 @@
-export { boot } from "./beam";
 export { PopcornError } from "./errors";
 export { Popcorn } from "./popcorn";
 export type { PopcornOpts, PopcornSendOpts } from "./popcorn";
 export type { PopcornEvent } from "./events";
-export type {
-  BeamBootOptions,
-  BeamEvent,
-  BeamTarget,
-  OtpErrorPayload,
-} from "./types";
+export type { BeamTarget, OtpErrorPayload } from "./types";
 export type { PopcornErrors, SerializedError } from "./errors";
