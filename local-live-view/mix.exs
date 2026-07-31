9046851dffc7792f88f207a4736452691f3baf83
Franciszek Kubis <franciszek.kubis@swmansion.com>
2026-07-31 13:05:04 +0200

    Moved to specific popcorn version for the sake of outside of monorepo llv usage.

--- local-live-view/mix.exs
+++ local-live-view/mix.exs
@@ -31,8 +31,8 @@ defmodule LocalLiveView.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:popcorn, "~> 0.3.0-rc2", targets: :wasm},
-      {:popcorn, path: "../popcorn/elixir", targets: :wasm},
+      {:popcorn, "~> 0.3.0-rc2", targets: :wasm},
+      # {:popcorn, path: "../popcorn/elixir", targets: :wasm},
       {:ex_doc, "~> 0.34", only: [:dev, :test], runtime: false, warn_if_outdated: true},
       {:igniter, ">= 0.7.0", targets: :host, runtime: false},
       {:playwright,
@@ -63,7 +63,6 @@ defmodule LocalLiveView.MixProject do
       ],
       groups_for_extras: [
         Introduction: ~r"/introduction/",
-        "Getting started": "README.md",
         Guides: ~r"/guides/"
       ]
     ]
