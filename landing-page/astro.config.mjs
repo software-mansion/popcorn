42f19e8c0ebcb8df21cdc6d5f1a388421dc815dd
Jakub Gonet <jakub.gonet@swmansion.com>
2026-08-10 23:27:27 +0200

    landing: use builtin devtoolsJson()

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-import devtoolsJson from "vite-plugin-devtools-json";
 import { popcorn } from "@swmansion/popcorn/vite";
 import {
   buildBundle,
@@ -26,9 +25,11 @@ export default defineConfig({
       "Cross-Origin-Resource-Policy": "cross-origin",
     },
   },
+  experimental: {
+    chromeDevtoolsWorkspace: true,
+  },
   vite: {
     plugins: [
-      devtoolsJson(),
       tailwindcss(),
       popcorn({
         rootDir: "../examples/iex-wasm",
