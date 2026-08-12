b530f554b954cbc698b89cd534f9675ffeb1c12e
Mateusz Front <mateusz.front@swmansion.com>
2026-08-12 11:13:49 +0200

    Deploy kanban and burrito as a single demo

--- local-live-view/assets/local_live_view/index.ts
+++ local-live-view/assets/local_live_view/index.ts
@@ -340,7 +340,7 @@ export class LLVEngine {
     const Socket = this.socketClass();
     const csrfToken = document.querySelector("meta[name='csrf-token']")?.getAttribute("content");
 
-    const llvSocket = new Socket("/llv_socket", {
+    const llvSocket = new Socket(this.config.llvSocketURI ?? "/llv_socket", {
       params: { _csrf_token: csrfToken },
     });
     llvSocket.connect();
